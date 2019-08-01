ee284260bbb175c39bcbb792afb82c6b6c8e3ae1
roshinijagan <53518760+roshinijagan@users.noreply.github.com>
2019-08-01 10:02:49 +0530

    Update RunCukesTest.java

--- src/test/java/skeleton/RunCukesTest.java
+++ src/test/java/skeleton/RunCukesTest.java
@@ -5,6 +5,6 @@ import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin = {"json.target\\jsonreport.json"})
+@CucumberOptions(plugin = {"json:target\\jsonreport.json"})
 public class RunCukesTest {
 }
